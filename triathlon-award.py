5edcbaae78da4cb6712baac6b12b843b9942a5ef
Khatri-D <khatri.dee@outlook.com>
2024-03-20 21:22:56 +0000

    Change variable names

--- triathlon-award.py
+++ triathlon-award.py
@@ -1,13 +1,12 @@
-
 # Get input for swimming, cycling, and running times
-swimming = input("Enter swimming time (minutes): ")
-cycling = input("Enter cycling time (minutes): ")
-running = input("Enter running time (minutes): ")
+swimming_input = input("Enter swimming time (minutes): ")
+cycling_input = input("Enter cycling time (minutes): ")
+running_input = input("Enter running time (minutes): ")
 
 # Convert the input from the user into integer
-swimming_time = int(swimming)
-cycling_time = int(cycling)
-running_time = int(running)
+swimming_time = int(swimming_input)
+cycling_time = int(cycling_input)
+running_time = int(running_input)
 
 # Calculate total time for the triathlon
 total_time = swimming_time + cycling_time + running_time
@@ -21,5 +20,3 @@ elif total_time <= 110:
     print(f"Your total time is {total_time} minutes, you receive 'Provincial Scroll.'")
 else:
     print(f"Your total time is {total_time} minutes, you receive 'No award.'")
-
-
